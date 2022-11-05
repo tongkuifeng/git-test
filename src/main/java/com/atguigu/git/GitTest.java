3860e5cd87b97dd455d62fa1e2895aaf62111a05
tkf <kuiluojun@163.com>
2022-11-05 11:36:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author tkf
+ * @create 2022/11/5 11:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
