6d3ad1e1a0ad359c8bc2f55966ed37b9c0721a4f
tkf <kuiluojun@163.com>
2022-11-05 15:08:49 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4 hot-fix!");
         System.out.println("hello git5 master!");
         System.out.println("hello git4 hot-fix!2");
+        System.out.println("hello git4 test11!!");
     }
 }
